a4b23a166297d68cdf4ea3e8e10311269978be58
MSK <hello@msk.io>
2018-06-14 02:28:01 +0100

    Run playbooks on Ubuntu 18.04

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-MACHINE_REPO=https://github.com/markosamuli/macos-machine.git
+MACHINE_REPO=https://github.com/markosamuli/linux-machine.git
 MACHINE=$HOME/.machine
 
 function download_machine {
     # Do we need to download the machine repository?
     if [ ! -d "$MACHINE" ]; then
-        echo "*** Cloning macos-machine from GitHub..."
+        echo "*** Cloning linux-machine from GitHub..."
         git clone $MACHINE_REPO $MACHINE
     fi
 }
 
-function setup_xcode {
-    command -v xcode-select 1>/dev/null 2>&1 || {
-        echo "Xcode is not installed."
+function setup_git {
+    command -v git 1>/dev/null 2>&1 || {
+        echo "Git is not installed."
         exit 1
     }
 }
@@ -23,11 +23,11 @@ function setup_machine {
     ./setup
 }
 
+# Git setup
+setup_git
+
 # Clone repository
 download_machine
-
-# Xcode setup
-setup_xcode
 
 # Run setup
 setup_machine
